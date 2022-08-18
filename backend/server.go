08ac35b126c2d1a45e9d155cf305236b8b62cd3c
Sylvanus Kateile <kateile@outlook.com>
2022-08-18 21:25:16 +0300

    apply mime type fix

--- backend/server.go
+++ backend/server.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
 func main() {
@@ -17,37 +15,22 @@ func main() {
 	}
 
 	router := chi.NewRouter()
-
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/javascript")
+			next.ServeHTTP(w, r)
+		})
+	})
 	//Status router for testing if server is working
 	router.Get("/status", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte("It is working!!!!!!!!!!!!!!!!!"))
 	})
 
 	//Configuring frontend
-	workDir, _ := os.Getwd()
-	filesDir := http.Dir(filepath.Join(workDir, "web")) //using "test" here works
-	FileServer(router, "/", filesDir)
+	fs := http.FileServer(http.Dir("web"))
+
+	router.Handle("/*", http.StripPrefix("/", fs))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe("127.0.0.1:"+port, router))
-}
-
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-
-	path += "*"
-
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
 }
